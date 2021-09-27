ac9aed37a1b21dc9f371e3ef88255fdc76375c24
Jon Morgan <jonmorg@iinet.net.au>
2021-09-28 09:58:30 +1000

    added populating select with country from geojson and listeners and displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,41 @@
+var destination;
+var favourites = [];
+if(localStorage.getItem("favourites")==null){
+favourites = ['New Zealand','Canada'];
+} else {
+favourites = JSON.parse(localStorage.getItem("favourites"));    
+}
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 3);
 map.zoomControl.setPosition('bottomright');
 
-if(navigator.onLine){
 var baselayer = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', { maxZoom: 22,attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
-}
 
 $.getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson', function(data) {
   L.geoJson(data,{
     style: areaStyle,
     onEachFeature: onEachFeature
 }).addTo(map);
+$('#country_from').val('Australia');
 });
 
 function onEachFeature(feature, layer) {
+    $('#country_from').append("<option value='"+feature.properties.name+"'>"+feature.properties.name+"</option>");
+    $('#country_to').append("<option value='"+feature.properties.name+"'>"+feature.properties.name+"</option>");
     layer.on({
         click: whenClicked
     });
 }
 
 function whenClicked(e) {
-
-  alert(e.target.feature.properties.name);
+  $('#country_to').val(e.target.feature.properties.name);
+  destination = e.target.feature.properties.name;
+  //RUN COVID API AND DISPLAY RESPONSE
+  $('#result').empty().append("<h2>"+e.target.feature.properties.name+"</h2>").show();
+  if(favourites.includes(e.target.feature.properties.name)){} else {
+    $("#confirm_text").html("Do you wish to add "+e.target.feature.properties.name+" to your favourites");
+    $("#confirm").show(); 
+ }
 }
 
 function areaStyle(feature){
@@ -33,4 +47,34 @@ function areaStyle(feature){
     color: '#ff0000',
   }
 };
+
+$("#country_to").on("change",function(){
+    $('#result').empty().append("<h2>"+$(this).val()+"</h2>").show();
+    destination = $(this).val();
+    if(favourites.includes($(this).val())){} else {
+       $("#confirm_text").html("Do you wish to add "+$(this).val()+" to your favourites");
+       $("#confirm").show(); 
+    }
+});
+
+$("#fav").on("click",function(){
+  $("#favourites").show();
+});
+
+for(var i=0;i<favourites.length;i++){
+    $("#favourites").append("<div class='favs'>"+favourites[i]+"</div>"); 
+}
+
+$("#save").on("click",function(){
+    favourites.push(destination);
+    localStorage.setItem("favourites",JSON.stringify(favourites));
+  });
+
+  $("#cancel").on("click",function(){
+    $("#confirm").hide();
+  });
+
+  $(".favs").on("click",function(){
+    $('#result').empty().append("<h2>"+$(this).html()+"</h2>").show(); 
+  })
   
