ee1fc2ec3dd3f80d049098668243db6eb7145920
Jon Morgan <jonmorg@iinet.net.au>
2021-09-28 10:51:38 +1000

    fixing leaflet http:// issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ favourites = JSON.parse(localStorage.getItem("favourites"));
 var map = L.map('map').setView([51.505, -0.09], 3);
 map.zoomControl.setPosition('bottomright');
 
-var baselayer = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', { maxZoom: 22,attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+var baselayer = L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', { maxZoom: 22,attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
 
 $.getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson', function(data) {
   L.geoJson(data,{
